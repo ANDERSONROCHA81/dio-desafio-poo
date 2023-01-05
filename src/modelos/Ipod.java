96104eb0abbd392887fb095ac5c1dd7bf2c56d84
ANDERSONROCHA81 <ironmanrocha81@gmail.com>
2023-01-05 17:09:28 -0300

    Criação do pacote modelos e da classe Ipod com sua implementação

---
+++ src/modelos/Ipod.java
@@ -0,0 +1,20 @@
+package modelos;
+
+import interfaces.ReprodutorMusical;
+
+public class Ipod implements ReprodutorMusical {
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música no Ipod!");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando música no Ipod!");
+    }
+
+    @Override
+    public void selecionarMusica() {
+        System.out.println("Selecionando música no Ipod!");
+    }
+}
